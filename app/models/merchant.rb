c911eb5248d8da4a7f1b249b06dd40ee2acddcbb
Tracy Kaneshiro <tkaneshiro@gmail.com>
2016-02-29 13:24:08 -0800

    fixed regexp for merchant

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,5 +1,5 @@
 class Merchant < ActiveRecord::Base
-	VALID_MERCHANT_NAME_REGEX = /\A[a-z\d]+[-\s]*/i   # this is not right still
+	VALID_MERCHANT_NAME_REGEX = /\A[a-z\d]+[-\sa-z\d]*\z/i
 	validates(:name, presence: true, length: {maximum: 50}, 
 		format: { with: VALID_MERCHANT_NAME_REGEX }, 
 		uniqueness: { case_sensitive: false})
