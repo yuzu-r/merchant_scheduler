142a05679893eb8281194fdb566dc5805a63d54e
Tracy Kaneshiro <tkaneshiro@gmail.com>
2016-03-05 15:02:30 -0800

    add user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160228200334) do
+ActiveRecord::Schema.define(version: 20160305223013) do
 
   create_table "merchants", force: :cascade do |t|
     t.string   "name"
@@ -20,5 +20,15 @@ ActiveRecord::Schema.define(version: 20160228200334) do
   end
 
   add_index "merchants", ["name"], name: "index_merchants_on_name", unique: true
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
