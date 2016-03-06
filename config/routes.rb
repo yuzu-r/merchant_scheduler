959154f63dc8a382f3ff615fe5e65136e21bd223
Tracy Kaneshiro <tkaneshiro@gmail.com>
2016-03-05 22:38:10 -0800

    8.1-create session controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   
-  #get 'users/new'
-
+  root 								'static_pages#home'
+  get 'help'	 		=> 	'static_pages#help'
+  get 'login'			=>	'sessions#new'
+  post 'login'		=>	'sessions#create'
+  delete 'login'	=>	'sessions#destroy'
+  
   resources :merchants
   resources :users
-  get 'help' => 'static_pages#help'
-  root 'static_pages#home'
-
 
 end
