2aa31bc57c5025d6ee6bd966c046ea49764aac16
Tracy Kaneshiro <tkaneshiro@gmail.com>
2016-03-09 19:36:42 -0800

    complete log-in-log-out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,9 +5,8 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      logger.debug "about to log in user"
       log_in user
-      logger.debug "login worked, about to redirect"
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       #after_login_dumass
       redirect_to help_path
     else
@@ -18,7 +17,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_path
   end
 
