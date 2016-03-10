881b99f2df7575e63e36bf12e0b50e4bee69f9a5
Tracy Kaneshiro <tkaneshiro@gmail.com>
2016-03-09 20:27:50 -0800

    add user index, account menu, edit user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,14 @@ class UsersController < ApplicationController
 	  end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def index
+  	@users = User.all
+  end
+
 	private
 		def user_params
 			params.require(:user).permit(:name, :email, :password, :password_confirmation)
