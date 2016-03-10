c63acac7b3668d63ce0de1a31e79c5c15a153934
Tracy Kaneshiro <tkaneshiro@gmail.com>
2016-03-09 18:42:05 -0800

    8.3-logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -18,7 +18,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    # do things?
+    log_out
     redirect_to root_path
   end
 
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -29,4 +29,22 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert is_logged_in? 
   end
 
+  test "login with valid information then logout" do
+    get login_path
+    assert_template 'sessions/new'
+    post login_path, session: { email: @user.email, password: 'password' }
+    assert is_logged_in?
+    assert_redirected_to help_path
+    follow_redirect! 
+		assert_template 'static_pages/help'
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", logout_path
+    delete logout_path
+    assert_not is_logged_in?
+    assert_redirected_to root_path
+    follow_redirect!
+    assert_select "a[href=?]", login_path
+    assert_select "a[href=?]", logout_path, count: 0
+  end
+
 end
