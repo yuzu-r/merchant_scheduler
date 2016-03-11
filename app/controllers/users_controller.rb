db80346a615af4d6879f521a284b0b37859d66a9
Tracy Kaneshiro <tkaneshiro@gmail.com>
2016-03-10 17:49:31 -0800

    9.2.2 - ensuring correct user for edits

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,13 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user, only: [:edit, :update]
+
   def new
   	@user = User.new
   end
 
   def show
-  	@user = User.find(params[:id])
+  	#@user = User.find(params[:id]) #this is now in before action?
   end
 
   def create	 
@@ -18,11 +21,11 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
+    #@user = User.find(params[:id]) #this is now in before action
   end
 
   def update
-    @user = User.find(params[:id])
+    #@user = User.find(params[:id]) #this is now in before action
     if @user.update_attributes(user_params)
       flash[:success] = "User updated"
       redirect_to @user
@@ -40,4 +43,21 @@ class UsersController < ApplicationController
 		def user_params
 			params.require(:user).permit(:name, :email, :password, :password_confirmation)
 		end
+
+    # before filters
+
+    # confirms a logged-in user 
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
+
+    # confirms a correct user
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
+
 end
