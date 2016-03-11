02adeecd869973ab00bb44560f19f12644d6c74c
Tracy Kaneshiro <tkaneshiro@gmail.com>
2016-03-10 18:25:48 -0800

    end of 9.2

--- test/integration/users_create_test.rb
+++ test/integration/users_create_test.rb
@@ -1,6 +1,11 @@
 require 'test_helper'
 
 class UsersCreateTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @user = users(:ridiculous)
+    host! "localhost:3000" #if you do not do this, it will go to example.com/help
+  end
   
   test "check for invalid information" do
   	get new_user_path
@@ -14,12 +19,13 @@ class UsersCreateTest < ActionDispatch::IntegrationTest
   end
 
   test "create user with valid information" do
+    log_in_as(@user)
   	get new_user_path
   	assert_difference 'User.count', 1 do
-  		post_via_redirect users_path, user: {name: "Example User", 
-  																				email: "example.user@example.com",
-  																				password: "password",
-  																				password_confirmation: "password" }
+  	post_via_redirect users_path, user: {name: "Example User", 
+  																			email: "example.user@example.com",
+  																			password: "password",
+  																			password_confirmation: "password" }
 		end
 		assert_template 'users/index'
 		assert_not flash.empty?
