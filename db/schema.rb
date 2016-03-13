a90fc4653ec0a8e7a41b43b8a9a4480be8289d61
Tracy Kaneshiro <tkaneshiro@gmail.com>
2016-03-13 12:31:09 -0700

    password resets

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160313060757) do
+ActiveRecord::Schema.define(version: 20160313190248) do
 
   create_table "merchants", force: :cascade do |t|
     t.string   "name"
@@ -32,6 +32,8 @@ ActiveRecord::Schema.define(version: 20160313060757) do
     t.string   "activation_digest"
     t.boolean  "activated",         default: false
     t.datetime "activated_at"
+    t.string   "reset_digest"
+    t.datetime "reset_sent_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
